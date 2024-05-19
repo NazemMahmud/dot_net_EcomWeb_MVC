c7df8f691ba96c01db6a22af87258d93ccbb6945
Piash <nazem.piash10@gmail.com>
2024-05-19 20:17:16 +0600

    1. seed: for category table added 2. category page created and added in header 3. get all category list in controller

--- EcomWebLocal/Data/ApplicationDbContext.cs
+++ EcomWebLocal/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace EcomWebLocal.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Thriller", DisplayOrder = 3 }
+                );
+        }
     }
 }
