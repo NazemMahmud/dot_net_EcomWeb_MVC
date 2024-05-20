f704ef06416a6f8450106d4731a9b71fc5b3cea8
Piash <nazem.piash10@gmail.com>
2024-05-21 02:52:10 +0600

    edit-delete-tempdata: 1. categpry edit and delete operation success 2. add Tempdata and upon success Db operation return index page with a    success message

--- EcomWebLocal/Controllers/CategoryController.cs
+++ EcomWebLocal/Controllers/CategoryController.cs
@@ -22,22 +22,88 @@ namespace EcomWebLocal.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            if (category.Name == category.DisplayOrder.ToString()) 
+            if (category.Name == category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name", "DisplayOrder can't exactly match with name");    
+                ModelState.AddModelError("name", "DisplayOrder can't exactly match with name");
             }
 
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "A Category Is Created Successfully.";
                 return RedirectToAction("Index", "Category");
             }
 
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id); // this only works for ID
+            // other 2 ways of query
+            //Category category1 = _db.Categories.FirstOrDefault(cat => cat.Id == id);
+            //Category category2 = _db.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "A Category Is Updated Successfully.";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "A Category Is Deleted Successfully.";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
