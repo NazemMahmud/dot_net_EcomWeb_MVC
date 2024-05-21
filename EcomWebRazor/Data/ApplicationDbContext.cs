5d3cff8b220facf3d7b9b89927553d4e77a4e4c8
Piash <nazem.piash10@gmail.com>
2024-05-21 19:41:44 +0600

    razor-init-db_seed 1. razor project initialize 2. package tools install and db connected 3. run category seed

---
+++ EcomWebRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EcomWebRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcomWebRazor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Thriller", DisplayOrder = 3 }
+                );
+        }
+    }
+}
