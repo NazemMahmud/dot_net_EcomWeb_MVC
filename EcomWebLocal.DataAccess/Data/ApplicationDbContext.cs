eb94c067ea02dc9cdf49c04ebdd82b68f6a03171
Piash <nazem.piash10@gmail.com>
2024-05-24 20:39:40 +0600

    6-product-crud-viewbag-viewmodels 1. add: product model and update: applicationdbcontext with seed 2. add: repository and unit of work for product

--- EcomWebLocal.DataAccess/Data/ApplicationDbContext.cs
+++ EcomWebLocal.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace EcomWebLocal.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,81 @@ namespace EcomWebLocal.DataAccess.Data
                 new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "Thriller", DisplayOrder = 3 }
                 );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Harry Potter and the Sorcerer’s Stone",
+                    Author = "J.K. Rowling",
+                    Description = "Harry Potter, the boy who lived",
+                    ISBN = "9781781106501",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+               new Product
+               {
+                   Id = 2,
+                   Title = "Harry Potter and the Sorcerer’s Stone",
+                   Author = "J.K. Rowling",
+                   Description = "Harry Potter, the boy who lived",
+                   ISBN = "9781781106501",
+                   ListPrice = 40,
+                   Price = 30,
+                   Price50 = 25,
+                   Price100 = 20
+               },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Harry Potter and the Sorcerer’s Stone",
+                    Author = "J.K. Rowling",
+                    Description = "Harry Potter, the boy who lived",
+                    ISBN = "9781781106501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Harry Potter and the Sorcerer’s Stone",
+                    Author = "J.K. Rowling",
+                    Description = "Harry Potter, the boy who lived",
+                    ISBN = "9781781106501",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Harry Potter and the Sorcerer’s Stone",
+                    Author = "J.K. Rowling",
+                    Description = "Harry Potter, the boy who lived",
+                    ISBN = "9781781106501",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Harry Potter and the Sorcerer’s Stone",
+                    Author = "J.K. Rowling",
+                    Description = "Harry Potter, the boy who lived",
+                    ISBN = "9781781106501",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20
+                }
+               );
         }
     }
 }
---
+++ EcomWebLocal.DataAccess/Repositories/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcomWebLocal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomWebLocal.DataAccess.Repositories.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
